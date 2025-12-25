a184e66a0365431abc1248048c22c2d99e4fbe22
전원준 <jwjjwj1960@naver.com>
2025-12-25 18:44:34 +0900

    feat: Input,Output Handler 생성

---
+++ src/main/java/calculator/view/InputHandler.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputHandler {
+    public static String readInput() {
+        return readLine();
+    }
+}
---
+++ src/main/java/calculator/view/OutputHandler.java
@@ -0,0 +1,7 @@
+package calculator.view;
+
+public class OutputHandler {
+    public static void printResult(int output) {
+        System.out.printf("결과 : %d", output);
+    }
+}
